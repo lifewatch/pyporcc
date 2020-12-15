745d2f9497bae3099cd3f794ae702afc72aae66f
unknown <Administrator@LT8Q05YY1.vliz.be>
2020-12-15 14:31:28 +0100

    Terminal runnable convert/clicks example

--- examples/convert_clicks.py
+++ examples/convert_clicks.py
@@ -1,29 +1,31 @@
 
-
+import argparse
 from pyporcc import click_detector
-
 import pyhydrophone as pyhy
 
+def run_detect_clicks():
+    parser = argparse.ArgumentParser(description="""""")
+    parser.add_argument('-sfol', '--sound_folder_path',
+                        type=str,
+                        help='Path to sound files folder')
+    parser.add_argument('-input', '--click_model_path',
+                        type=str,
+                        help='Path to click model')
+    parser.add_argument('-o', '--output',
+                        type=str,
+                        help='Output path for the detected clicks')
+    args = parser.parse_args()
 
-##############
-# CONFIG
+    sound_folder_path = args.sound_folder_path
+    click_model_path = args.click_model_path
+    output = args.output
 
-# Sound Files
-sound_folder_path = "//archive/other_platforms/soundtrap/2017/najaar2017_reefballs_Belwind/67416073.170809"
-
+## CONFIG
 name = 'SoundTrap'
 model = 1
 serial_number = 67416073
 Vpp = 2
 hydrophone = pyhy.SoundTrapHF(name, model, serial_number, Vpp)
-
-
-# Click model
-click_model_path = '../pyporcc/data/standard_click.wav'
-
-# Output path for the detected clicks
-clicks_output_path = "test.csv"
-
 
 if __name__ == "__main__":
     """
@@ -32,4 +34,4 @@ if __name__ == "__main__":
     # Convert the sound clips to click and save
     clicks_df = hydrophone.read_HFclicks(sound_folder_path)
     converter = click_detector.ClickConverter(click_model_path)
-    clicks = converter.clicks_df(clicks_df, save_path=clicks_output_path)
+    clicks = converter.clicks_df(clicks_df, save_path=output)
